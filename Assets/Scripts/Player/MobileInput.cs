a06cb8f4b3d6e58a0019180bb1bec4b058af2238
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-05-19 15:48:38 +0300

    Import sprites, add player, joystick scripts, add UI

---
+++ Assets/Scripts/Player/MobileInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MobileInput : MonoBehaviour
+{
+    public JoyStick MovejoyStick;
+    public JoyStick LookjoyStick;
+
+    void Update()
+    {
+        var fps = GetComponent<CharacterControl>();
+        fps.RunAxis = MovejoyStick.InputDirection;
+        fps.LookAxis = LookjoyStick.InputDirection;
+
+    }
+}
