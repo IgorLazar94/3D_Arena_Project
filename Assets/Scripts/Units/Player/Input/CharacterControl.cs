a29f86b8227ee8d4e529b938f9c21a8c22f2f3ea
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-06-29 13:02:31 +0300

    Remove unnecessary code

--- Assets/Scripts/Units/Player/Input/CharacterControl.cs
+++ Assets/Scripts/Units/Player/Input/CharacterControl.cs
@@ -11,12 +11,6 @@ public class CharacterControl : MonoBehaviour
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
     public float jumpHeight = 3f;
-    //private float normalSpeed = 7.5f;
-    //private float crouchSpeed = 4.0f;
-
-    //private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
-    //private Vector3 playerScale = new Vector3(1, 1, 1);
-
     [HideInInspector]
     public Vector2 RunAxis;
     [HideInInspector]
@@ -29,30 +23,10 @@ public class CharacterControl : MonoBehaviour
     [HideInInspector]
     public bool canMove = true;
 
-    //[Header("ButtonAssign")]
-    //public GameObject walk_Btn;
-    //public GameObject jump_Button;
-    //public GameObject crouch_Button;
-    //public GameObject stand_button;
-
-    //private Footsteps footsteps;
-    //private float walk_volum_min = 0.2f;
-    //private float walk_volum_max = 0.6f;
-    //private float walk_step_distance = 0.4f;
-
-    private void Awake()
-    {
-        //footsteps = GetComponentInChildren<Footsteps>();
-    }
-
-
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         walkingSpeed = GameSettings.Instance.GetPlayerMoveSpeed();
-        //footsteps.Volum_max = walk_volum_max;
-        //footsteps.Volum_min = walk_volum_min;
-        //footsteps.step_Distance = walk_step_distance;
     }
 
     void Update()
@@ -69,16 +43,9 @@ public class CharacterControl : MonoBehaviour
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
-            //jump_Button.SetActive(false);
         }
-        else
-        {
-            //jump_Button.SetActive(true);
-        }
-       
         characterController.Move(moveDirection * Time.deltaTime);
 
-        
         if (canMove)
         {
             rotationX += -LookAxis.y * lookSpeed;
@@ -88,38 +55,4 @@ public class CharacterControl : MonoBehaviour
         }
     }
 
-    //public void Jump()
-    //{
-    //    moveDirection.y = jumpHeight;
-    //}
-
-    //public void Run()
-    //{
-    //    walkingSpeed = runningSpeed;
-    //    walk_Btn.SetActive(true);
-    //}
-
-    //public void Walk()
-    //{
-    //    walkingSpeed = normalSpeed;
-    //    walk_Btn.SetActive(false);
-    //}
-
-    //public void Crouch()
-    //{
-    //    transform.localScale = crouchScale;
-    //    transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-    //    walkingSpeed = crouchSpeed;
-    //    stand_button.SetActive(true);
-    //    crouch_Button.SetActive(false);
-    //}
-
-    //public void Stand()
-    //{
-    //    transform.localScale = playerScale;
-    //    transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
-    //    walkingSpeed = crouchSpeed;
-    //    stand_button.SetActive(false);
-    //    crouch_Button.SetActive(true);
-    //}
 }
--- Assets/Scripts/Units/Player/Input/MobileInput.cs
+++ Assets/Scripts/Units/Player/Input/MobileInput.cs
@@ -8,13 +8,6 @@ public class MobileInput : MonoBehaviour
     public JoyStick MovementJoyStick;
     public JoyStick LookedJoystick;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         var fps = GetComponent<CharacterControl>();
